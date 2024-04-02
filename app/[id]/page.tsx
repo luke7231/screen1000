fcc808bcb42d368bfb3655a6d34519a85c6ac610
luke7231 <tmddnjs7231@gmail.com>
2024-04-02 13:18:27 +0900

    chore: 삭제된 데이터 참조 코드 제거

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,14 +1,11 @@
 import Image from 'next/image';
-import Img1 from '../../public/snowflake.jpg';
 import Link from 'next/link';
 
 export default function Screen() {
     return (
         <main>
             <section className="max-w-[1200px] mx-auto flex min-h-screen mt-8">
-                <div className="mb-12">
-                    <Image alt="landing page" src={Img1} width={830}></Image>
-                </div>
+                <div className="mb-12"></div>
                 <div className="order-1 shrink-0 md:basis-[340px] md:w-[340px] md:ml-[30px]">
                     <div className="sticky top-6">
                         <Link href={'./'} className="font-light">
