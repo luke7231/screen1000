ae92578ab35d8bf7fe8ca2136140770c84858149
luke7231 <tmddnjs7231@gmail.com>
2024-04-09 14:25:06 +0900

    feat: 상세페이지 반응형 구조 추가

--- app/screen/[id]/page.tsx
+++ app/screen/[id]/page.tsx
@@ -12,10 +12,10 @@ export default async function Screen({ params: { id } }: Props) {
 
     return (
         <main className="grow">
-            <section className="max-w-[1200px] mx-auto flex mt-8">
-                <div className="mb-12 w-[830px] h-full">
+            <section className="max-w-[1200px] grow mx-auto flex flex-col md:flex-row mt-8">
+                <div className="mb-12 w-full h-full mx-auto pl-6 pr-6 md:pr-0 ">
                     {/* eslint-disable-next-line @next/next/no-img-element */}
-                    <img src={page.image} className="w-full" alt="screen" />
+                    <img src={page.image} className="w-full shadow-md" alt="screen" />
                 </div>
                 <div className="order-1 shrink-0 md:basis-[340px] md:w-[340px] md:ml-[30px]">
                     <div className="sticky top-6">
