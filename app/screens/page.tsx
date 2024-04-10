ebc8595a2e3e65c743612bc06f8b0283a2f1f425
luke7231 <tmddnjs7231@gmail.com>
2024-04-10 23:34:59 +0900

    feat: screens page 마진,패딩 수정

--- app/screens/page.tsx
+++ app/screens/page.tsx
@@ -16,8 +16,12 @@ export default async function Home({
     const totalPages = await getNumberOfPage(tagKey);
     return (
         <main className="max-w-[1200px] mx-auto flex min-h-screen flex-col items-center">
-            <Filter />
-            <ImageGrid tagKey={tagKey} currentPage={currentPage} />
+            <div className="py-5 md:py-8 sticky top-0 bg-[#fafafa] w-full flex justify-center">
+                <Filter />
+            </div>
+            <div className="mt-4 md:mt-8">
+                <ImageGrid tagKey={tagKey} currentPage={currentPage} />
+            </div>
 
             <div className="flex w-full justify-center mt-12 mb-12">
                 <Pagination totalPages={totalPages} />
