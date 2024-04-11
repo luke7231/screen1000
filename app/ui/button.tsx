23afc9b5fda0658382756facf5098251168d6eb3
luke7231 <tmddnjs7231@gmail.com>
2024-04-11 22:35:29 +0900

    chore: css 간단 수정(button 컴포넌트 -> basis)

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Button = ({ text, href }: { text: string; href: string }) => {
     return (
-        <div className="order-1 shrink-0 md:basis-[340px] md:w-[340px] md:ml-[30px]">
+        <div className="order-1 shrink-0 basis-[340px] md:w-[340px] md:ml-[30px]">
             <div className="sticky top-6 pl-6 pr-6 md:pl-0">
                 <Link href={href} className="font-light">
                     <div className="py-3 mb-4 w-full flex justify-center items-center bg-gray-800 text-white rounded-lg">
