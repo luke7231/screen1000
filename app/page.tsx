5aa2d39c4f1f546d63fc6d8c148013960bd2cc6f
luke7231 <tmddnjs7231@gmail.com>
2024-04-27 17:32:54 +0900

    feat: main 화면 상호명 변경

--- app/page.tsx
+++ app/page.tsx
@@ -18,14 +18,15 @@ export default async function Home({
     const totalPages = await getNumberOfPage(tagKey);
     return (
         <main className="max-w-[1200px] mx-auto flex min-h-screen flex-col items-center">
-            <h1 className="font-bold antialiased  text-[60px] md:text-[90px] lg:text-[120px]  text-center mt-40">
-                화면 1000
+            <h1 className="font-bold antialiased  text-[60px] md:text-[90px] lg:text-[100px]  text-center mt-14">
+                Screen Palette
             </h1>
-            <h2 className="font-light antialiased text-[16px] md:text-[18px] lg:text-[24px] text-gray-500 mt-8 md:mt-12 lg:mt-14">
-                1000개의 스크린으로 영감을 얻으세요
+            <h2 className="font-light antialiased text-[16px] md:text-[18px] lg:text-[24px] text-gray-500 mt-4 md:mt-6 lg:mt-8">
+                Look around the screen by color
             </h2>
-            <div className="pt-5">
+            <div className="mt-28">
                 <Filter />
+                <div className="text-center mt-4">click me!</div>
             </div>
             <div className="mt-24 md:mt-36 lg:mt-48">
                 <ImageGrid tagKey={tagKey} currentPage={currentPage} />
